7b230005b426871a6801abf9283787c7c3411636
김희정 <heejung030619@gmail.com>
2025-09-23 13:47:37 +0900

    Card 코드 줄임

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 #define MAX_SIZE 52
 #define CARD 3
+#define Member 2
 
 int main()
 {
-
     srand((unsigned int)time(nullptr));
     int Deck[MAX_SIZE];
 
@@ -23,53 +23,84 @@ int main()
     //Ä«µå ¼¯À½
     random_shuffle(&Deck[0], &Deck[51]);
 
-    int Dealer[3];
-    int Player[3];
+    int Dealer[3] = { 0,0,0 };
+    int Player[3] = { 0,0,0 };
 
+    string Shape[4] = { "¢¼", "¢À", "¢½", "¡Þ" };
     int i = 0;
-    //Ä«µå »ÌÀ½
-    for (i = 0; i < CARD; i++)
-    {
-        Dealer[i] = Deck[i];
 
-        Player[i] = Deck[i + CARD];
-    }
+    //Ä«µå »ÌÀ½
+    Dealer[0] = Deck[0];
+    Dealer[1] = Deck[2];
+    Dealer[2] = Deck[4];
+
+    Player[0] = Deck[1];
+    Player[1] = Deck[3];
+    Player[2] = Deck[5];
 
     for (int i = 0; i < CARD; i++)
     {
-        if (Dealer[i] % 4 == 0)
+        //Dealer Card Number
+        if (Dealer[i] / 4 + 1 == 11)
         {
-            cout << "Dealer : Spade " << (Dealer[i] / 4) +1 << "\t";
+            cout << "Dealer : " << Shape[Dealer[i] % 4];
+            cout << "J" << "\t";
         }
-        else if (Dealer[i] % 4 == 1)
+        else if (Dealer[i] / 4 + 1 == 12)
         {
-            cout << "Dealer : Clover " << (Dealer[i] / 4) + 1 << "\t";
+            cout << "Dealer : " << Shape[Dealer[i] % 4];
+            cout << "Q" << "\t";
         }
-        else if (Dealer[i] % 4 == 2)
+        else if (Dealer[i] / 4 + 1 == 13)
         {
-            cout << "Dealer : Heart " << (Dealer[i] / 4) + 1 << "\t";
+            cout << "Dealer : " << Shape[Dealer[i] % 4];
+            cout << "K" << "\t";
         }
         else
         {
-            cout << "Dealer : Dia " << (Dealer[i] / 4) + 1 << "\t";
+            cout << "Dealer : " << Shape[Dealer[i] % 4];
+            cout << (Dealer[i] / 4) + 1 << "\t";
         }
 
-        if (Dealer[i] % 4 == 0)
+        //Player Card Number
+        if (Player[i] / 4 + 1 == 11)
         {
-            cout << "Player : Spade " << (Player[i] / 4) + 1 << endl;
+            cout << "Player : " << Shape[Player[i] % 4];
+            cout << "J" << endl;
         }
-        else if (Dealer[i] % 4 == 1)
+        else if (Player[i] / 4 + 1 == 12)
         {
-            cout << "Player : Clover " << (Player[i] / 4) + 1 << endl;
+            cout << "Player : " << Shape[Player[i] % 4];
+            cout << "Q" << endl;
         }
-        else if (Dealer[i] % 4 == 2)
+        else if (Player[i] / 4 + 1 == 13)
         {
-            cout << "Player : Heart " << (Player[i] / 4) + 1 << endl;
+            cout << "Player : " << Shape[Player[i] % 4];
+            cout << "K" << endl;
         }
         else
         {
-            cout << "Player : Dia " << (Player[i] / 4) + 1 << endl;
+            cout << "Player : " << Shape[Player[i] % 4];
+            cout << (Player[i] / 4) + 1 << endl;
         }
+    }
+
+    int SumDealer = 0;
+    int SumPlaer = 0;
+    for (int i = 0; i < CARD; i++)
+    {
+        SumDealer += Dealer[i];
+
+        SumPlaer += Player[i];
+    }
+
+    if (SumDealer < SumPlaer)
+    {
+        cout << "Player Win" << endl;
+    }
+    else
+    {
+        cout << "Dealer Win" << endl;
     }
     
     return 0;
