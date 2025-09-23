02829080239e56e71d1b1604af92ba2ba683313c
김희정 <heejung030619@gmail.com>
2025-09-23 11:15:46 +0900

    Card

--- main.cpp
+++ main.cpp
@@ -1,27 +1,75 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 #include <algorithm>
 
 using namespace std;
 
 #define MAX_SIZE 52
+#define CARD 3
 
 int main()
 {
-    int pocket[MAX_SIZE];
 
+    srand((unsigned int)time(nullptr));
+    int Deck[MAX_SIZE];
+
+    //카드 만듦
     for (int i = 0; i < MAX_SIZE; i++)
     {
-        pocket[i] = i + 1;
+        Deck[i] = i + 1;
     }
 
-    //#include <algorithm>에 들어있는 기능임
-    random_shuffle(&pocket[0], &pocket[51]);
+    //#include <algorithm>에 들어있는 기능
+    //카드 섞음
+    random_shuffle(&Deck[0], &Deck[51]);
 
-    for (int i = 0; i < MAX_SIZE; i++)
+    int Dealer[3];
+    int Player[3];
+
+    int i = 0;
+    //카드 뽑음
+    for (i = 0; i < CARD; i++)
     {
-        cout << pocket[i] << endl;
+        Dealer[i] = Deck[i];
+
+        Player[i] = Deck[i + CARD];
+    }
+
+    for (int i = 0; i < CARD; i++)
+    {
+        if (Dealer[i] % 4 == 0)
+        {
+            cout << "Dealer : Spade " << (Dealer[i] / 4) +1 << "\t";
+        }
+        else if (Dealer[i] % 4 == 1)
+        {
+            cout << "Dealer : Clover " << (Dealer[i] / 4) + 1 << "\t";
+        }
+        else if (Dealer[i] % 4 == 2)
+        {
+            cout << "Dealer : Heart " << (Dealer[i] / 4) + 1 << "\t";
+        }
+        else
+        {
+            cout << "Dealer : Dia " << (Dealer[i] / 4) + 1 << "\t";
+        }
+
+        if (Dealer[i] % 4 == 0)
+        {
+            cout << "Player : Spade " << (Player[i] / 4) + 1 << endl;
+        }
+        else if (Dealer[i] % 4 == 1)
+        {
+            cout << "Player : Clover " << (Player[i] / 4) + 1 << endl;
+        }
+        else if (Dealer[i] % 4 == 2)
+        {
+            cout << "Player : Heart " << (Player[i] / 4) + 1 << endl;
+        }
+        else
+        {
+            cout << "Player : Dia " << (Player[i] / 4) + 1 << endl;
+        }
     }
     
     return 0;
