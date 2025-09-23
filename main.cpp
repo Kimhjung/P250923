729fa96ffc7740b8733b4e28e6edc822a339d693
김희정 <heejung030619@gmail.com>
2025-09-23 17:34:57 +0900

    포인터와 문자열

--- main.cpp
+++ main.cpp
@@ -3,92 +3,32 @@
 #include <algorithm>
 
 using namespace std;
-/* 
-// ___ Case 1 ___
-// 끝나면 그냥 없어짐
-void Swap(int A, int B)
-{
-    int Temp = A;
-    A = B;
-    B = Temp;
-}
 
-int main()
-{
-    int A = 10;
-    int B = 20;
-
-    //사용할 변수의 주소값을 넣음
-    Swap(A, B);
-
-    //반환 안됨
-    cout << A << endl;
-    cout << B << endl;
-
-    return 0;
-}
-
-// 출력값 : 10 '\n' 20
-*/
-
-/*
-// ___ Case2 ___
-//Array의 경우, 메모리 8byte 차지함
-//int, char 등의 데이터 타입은 4byte차지함
-//int, char 같은 경우 Pointer를 사용하지 않음
-void Swap(int* PA, int* PB)
-{
-    //Temp에 PA 주소의 값을 넣어줌
-    int Temp = *PA;
-    *PA = *PB;
-    *PB = Temp;
-}
-
-int main()
-{
-    int A = 10;
-    int B = 20;
-
-    //사용할 변수의 주소값을 넣음
-    Swap(&A, &B);
-
-    cout << A << endl;
-    cout << B << endl;
-
-    return 0;
-}
-*/
-
-// ___ Case 3 ___
-/*
-// C용
-void Swap(int* PA, int* PB)
-{
-    //Temp에 PA 주소의 값을 넣어줌
-    int Temp = *PA;
-    *PA = *PB;
-    *PB = Temp;
-}
-*/
-
-void Swap(int& PA, int& PB)
-{
-    //Temp에 PA 주소의 값을 넣어줌
-    int Temp = PA;
-    PA = PB;
-    PB = Temp;
-}
 
 int main() 
 {
-    int A = 10;
-    int B = 20;
+   int Number[10] = { 0,1,2,3,4,5 };
 
-    //사용할 변수의 주소값을 넣음
-    Swap(A, B);
+    int* P = Number;
 
-    cout << A << endl;
-    cout << B << endl;
+    /*
+    *P = 1;                    //P = 200
+    *(P + 1) = 2;           // P + 1 = 204(int = 4byte) //(0x200 + sizeof(int))
+    *(P + 2) = 3;
+    *(P + 3) = 4;
+    */
+
+    cout << *(Number + 1) << endl;
+    cout << Number[1] << endl;
+
+    //요즘 절대 안쓰는 법
+    cout << *(P++) << endl;     // 출력 0 -> P 출력 후 값 증가(증가된 값 원하면 ++P)
+    cout << *(P) << endl;
+
+    /*
+    delete P;     //P가 가리키는 곳 없애기
+    P = nullptr;    //P 비활성화
+    */
 
     return 0;
 }
