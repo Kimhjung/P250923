76a601e65b5d79fe62adc1e32c8e16c1dbc5e252
김희정 <heejung030619@gmail.com>
2025-09-23 15:22:28 +0900

    Pointer 기본

--- main.cpp
+++ main.cpp
@@ -4,104 +4,32 @@
 
 using namespace std;
 
-#define MAX_SIZE 52
-#define CARD 3
-#define Member 2
-
 int main()
 {
-    srand((unsigned int)time(nullptr));
-    int Deck[MAX_SIZE];
+    //Pointer
+    /*
+    //메모리 위치 잡기
+    int* P = new int;
 
-    //카드 만듦
-    for (int i = 0; i < MAX_SIZE; i++)
-    {
-        Deck[i] = i + 1;
-    }
+    //두개 같은거
+    int* P = 0;
+    int* P = nullptr;       //C++에만 있음
 
-    //#include <algorithm>에 들어있는 기능
-    //카드 섞음
-    random_shuffle(&Deck[0], &Deck[51]);
+    *P = 1;
+    */
 
-    int Dealer[3] = { 0,0,0 };
-    int Player[3] = { 0,0,0 };
+    /*포인터가 지워질 때
+    * 1. delete 했을 때
+    * 2. main 함수가 중단됐을 때
+    * 3. 컴퓨터가 다운됐을 때
+    */
+    bool* A = new bool;
 
-    string Shape[4] = { "♠", "♣", "♡", "◇" };
-    int i = 0;
+    *A = true;
 
-    //카드 뽑음
-    Dealer[0] = Deck[0];
-    Dealer[1] = Deck[2];
-    Dealer[2] = Deck[4];
+    cout << A;
 
-    Player[0] = Deck[1];
-    Player[1] = Deck[3];
-    Player[2] = Deck[5];
+    delete A;
 
-    for (int i = 0; i < CARD; i++)
-    {
-        //Dealer Card Number
-        if (Dealer[i] / 4 + 1 == 11)
-        {
-            cout << "Dealer : " << Shape[Dealer[i] % 4];
-            cout << "J" << "\t";
-        }
-        else if (Dealer[i] / 4 + 1 == 12)
-        {
-            cout << "Dealer : " << Shape[Dealer[i] % 4];
-            cout << "Q" << "\t";
-        }
-        else if (Dealer[i] / 4 + 1 == 13)
-        {
-            cout << "Dealer : " << Shape[Dealer[i] % 4];
-            cout << "K" << "\t";
-        }
-        else
-        {
-            cout << "Dealer : " << Shape[Dealer[i] % 4];
-            cout << (Dealer[i] / 4) + 1 << "\t";
-        }
-
-        //Player Card Number
-        if (Player[i] / 4 + 1 == 11)
-        {
-            cout << "Player : " << Shape[Player[i] % 4];
-            cout << "J" << endl;
-        }
-        else if (Player[i] / 4 + 1 == 12)
-        {
-            cout << "Player : " << Shape[Player[i] % 4];
-            cout << "Q" << endl;
-        }
-        else if (Player[i] / 4 + 1 == 13)
-        {
-            cout << "Player : " << Shape[Player[i] % 4];
-            cout << "K" << endl;
-        }
-        else
-        {
-            cout << "Player : " << Shape[Player[i] % 4];
-            cout << (Player[i] / 4) + 1 << endl;
-        }
-    }
-
-    int SumDealer = 0;
-    int SumPlaer = 0;
-    for (int i = 0; i < CARD; i++)
-    {
-        SumDealer += Dealer[i];
-
-        SumPlaer += Player[i];
-    }
-
-    if (SumDealer < SumPlaer)
-    {
-        cout << "Player Win" << endl;
-    }
-    else
-    {
-        cout << "Dealer Win" << endl;
-    }
-    
     return 0;
 }
